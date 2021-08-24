960e9ee52811f9c8be1e7ae4fcf6f624870c052c
Alan Fernandez <alanleonelfernandez@hotmail.com>
2021-08-24 18:29:36 -0300

    primer commit

---
+++ js/operaciones.js
@@ -0,0 +1,8 @@
+/* Desafio entegable 1 */
+
+    let pesos = parseInt(prompt("Ingrese cantidad de pesos"));
+    let dolarCompra = pesos / 179;
+    let dolarVenta = pesos / 182;
+
+    alert("Valor de compra del Dolar: " + dolarCompra);
+console.log("Valor de venta del Dolar: " + dolarVenta)
